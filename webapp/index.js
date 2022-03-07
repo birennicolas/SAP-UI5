c4441e1687406683fbfa018acf67786e1a6717c7
birennicolas <90913609+birennicolas@users.noreply.github.com>
2022-03-07 18:37:47 +0100

    view

--- webapp/index.js
+++ webapp/index.js
@@ -1,7 +1,13 @@
 sap.ui.define([
 
-], function () {
+    "sap/ui/core/mvc/XMLView"
+
+], function (XMLView) {
     "use strict";
 
-    alert("UI5 is ready to go!");
+    XMLView.create({
+        viewName: "sap.ui.demo.walkthrough.view.App"
+    }).then(function (oView) {
+        oView.placeAt("content");
+    });
 });
